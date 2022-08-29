8679c92b386d069f4bac816ffd1fcc2573418466
Aman Kumar Matta <amankumarmatta0@gmail.com>
2022-08-29 15:23:53 +0530

    Coded

---
+++ Assets/Scripts/Answer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Answer : MonoBehaviour
+{
+    public bool isCorrect = false;
+    public QuizManager quizmg;
+    public void Answers()
+    {
+        if (isCorrect)
+        {
+            Debug.Log("Correct");
+            quizmg.Correct();
+        }
+        else
+        {
+            Debug.Log("Wrong");
+            quizmg.Correct();
+        }
+    }
+}
---
+++ Assets/Scripts/QuizManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    public List<QuestionsAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQues;
+
+    public Text QuestionTxt;
+
+    private void Start()
+    {
+        generateQuestion();
+    }
+
+    public void Correct()
+    {
+        QnA.RemoveAt(currentQues);
+        generateQuestion();
+    }
+
+    void SetAnswers()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<Answer>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQues].Answers[i];
+
+            if(QnA[currentQues].CrctAnswer == i+1)
+            {
+                options[i].GetComponent<Answer>().isCorrect = true;
+            }
+        }
+    }
+
+    void generateQuestion()
+    {
+        currentQues = Random.Range(0, QnA.Count);
+        QuestionTxt.text = QnA[currentQues].Question;
+        SetAnswers();
+
+
+    }
+}
