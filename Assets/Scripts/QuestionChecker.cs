9c115e97a3c51ffd4a44e6598adcd27e1d5d4490
Aman Kumar Matta <amankumarmatta0@gmail.com>
2022-08-30 15:12:19 +0530

    minor changes

--- Assets/Scripts/QuestionChecker.cs
+++ Assets/Scripts/QuestionChecker.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestionChecker : MonoBehaviour
 {
-    public bool isCorrect = false;
+    public bool isCorrect;
     public QuizManager quizmg;
     public void QuestionCheck()
     {
         if (isCorrect)
-        {            
+        {             
             quizmg.correct();
+            Debug.Log("Correct");
         }
         else
         {            
